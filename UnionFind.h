1bb70184dc4de9e5abe2795a1b0a6f6430dff7f8
RomanUrmanov-stack <55079213+RomanUrmanov-stack@users.noreply.github.com>
2019-11-11 16:55:07 +0700

    Add files via upload

---
+++ UnionFind.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+#include <ctime>
+
+using namespace std;
+
+class Grid
+{
+public:
+	class UnionFind
+	{
+	public:
+		UnionFind(int N);
+		void un(int root_p, int root_c);
+		int find(int ind);
+
+		vector<int> u_f;
+	};
+
+	Grid(int N);
+
+	void process();
+	bool open(int row, int col);
+	bool perlocates(UnionFind &union_find) const;
+	void connect(int row, int col, UnionFind &union_find) const;
+	double get_threshold() const; 
+
+private:
+	vector<vector<int>> grid;
+	int op;
+	double threshold;
+};
+
+class GridStat
+{
+public:
+	double mean() const;
+	double stddev() const;
+	double confLo() const;
+	double confHi() const;
+
+	vector<double> thres_stat;
+};
